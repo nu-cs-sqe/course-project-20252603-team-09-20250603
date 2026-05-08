5bbd1fc8abd170cb09835969c82b326eff3596d7
crystalsychen <crystal.chen586@gmail.com>
2026-05-07 22:39:02 -0500

    helper function validatePlayerCount passed all tests

--- src/main/java/domain/GameInitializer.java
+++ src/main/java/domain/GameInitializer.java
@@ -1,4 +1,9 @@
 package domain;
 
 public class GameInitializer {
+    public void validatePlayerCount(int count){
+        if (count < 3 || count > 4){
+            throw new IllegalArgumentException("Catan requires players of 3 or 4");
+        }
+    }
 }
