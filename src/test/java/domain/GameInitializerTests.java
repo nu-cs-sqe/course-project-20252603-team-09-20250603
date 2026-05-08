14189d685c5a7f4b4ea2519a5ec26c51a8c8e65d
crystalsychen <crystal.chen586@gmail.com>
2026-05-07 23:47:43 -0500

    created tests for validatePlayerName()

--- src/test/java/domain/GameInitializerTests.java
+++ src/test/java/domain/GameInitializerTests.java
@@ -33,4 +33,45 @@ public class GameInitializerTests {
 
         assertThrows(IllegalArgumentException.class, () -> initializer.validatePlayerCount(5));
     }
+
+    @Test
+    void onlyFirstName_NoSpace_ValidName(){
+        GameInitializer initializer = new GameInitializer();
+
+        String name = "Cole";
+        assertDoesNotThrow(()->initializer.validatePlayerName(name));
+    }
+
+    @Test
+    void firstAndLastName_WithSpace_ValidName(){
+        GameInitializer initializer = new GameInitializer();
+
+        String name = "Alvin Li";
+        assertDoesNotThrow(()->initializer.validatePlayerName(name));
+    }
+
+    @Test
+    void blankName_InvalidName(){
+        GameInitializer initializer = new GameInitializer();
+
+        String name = "";
+        assertThrows(IllegalArgumentException.class, ()->initializer.validatePlayerName(name));
+    }
+
+    @Test
+    void whiteSpaceName_InvalidName(){
+        GameInitializer initializer = new GameInitializer();
+
+        String name = " ";
+        assertThrows(IllegalArgumentException.class, ()->initializer.validatePlayerName(name));
+    }
+
+    @Test
+    void nullName_InvalidName(){
+        GameInitializer initializer = new GameInitializer();
+
+        String name = null;
+        assertThrows(IllegalArgumentException.class, ()->initializer.validatePlayerName(name));
+    }
+
 }
