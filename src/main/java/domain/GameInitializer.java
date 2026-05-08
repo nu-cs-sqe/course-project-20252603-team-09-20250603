546576f913ba6ed9d04b3f3bc84005c0a0b59989
crystalsychen <crystal.chen586@gmail.com>
2026-05-08 00:06:57 -0500

    implemented assignColor() with enum and passed all tests

--- src/main/java/domain/GameInitializer.java
+++ src/main/java/domain/GameInitializer.java
@@ -1,6 +1,13 @@
 package domain;
 
 public class GameInitializer {
+    private static final PlayerColor[] COLORS = {
+            PlayerColor.RED,
+            PlayerColor.BLUE,
+            PlayerColor.ORANGE,
+            PlayerColor.WHITE
+    };
+
     public void validatePlayerCount(int count){
         if (count < 3 || count > 4){
             throw new IllegalArgumentException("Catan requires players of 3 or 4");
@@ -11,5 +18,13 @@ public class GameInitializer {
         if (name == null || name.trim().isBlank()){
             throw new IllegalArgumentException("Player name invalid, please input a name");
         }
+    }
+
+    public PlayerColor assignColor(int index){
+        if (index < 0 || index >= 4){
+            throw new IllegalArgumentException("Invalid player index for color assignment");
+        }
+
+        return COLORS[index];
     }
 }
