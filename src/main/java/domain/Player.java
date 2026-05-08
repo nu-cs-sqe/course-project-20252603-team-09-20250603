f1d14266bc0f746562d423bc1fb27928d5d0699e
crystalsychen <crystal.chen586@gmail.com>
2026-05-08 01:05:16 -0500

    getName implemented and passed tests

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,4 +1,29 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player {
+    private final int id;
+    private final String name;
+    private final PlayerColor color;
+    private final Map<String, Integer> inventory;
+    private int victoryPoints;
+    private boolean hasLongestRoad;
+    private boolean hasLargestArmy;
+
+    public Player(int id, String name, PlayerColor color){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.inventory = new HashMap<>();
+        this.inventory.put("roads", 15);
+        this.inventory.put("settlements", 5);
+        this.inventory.put("cities", 4);
+        this.victoryPoints = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
