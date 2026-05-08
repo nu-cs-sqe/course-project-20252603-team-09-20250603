96ce3aab1130ae4df6ebbcad3d898ea32fe2e965
crystalsychen <crystal.chen586@gmail.com>
2026-05-08 01:04:56 -0500

    getName test for player class created

--- src/test/java/domain/PlayerTests.java
+++ src/test/java/domain/PlayerTests.java
@@ -1,4 +1,21 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PlayerTests {
+    @Test
+    void getName_NoSpaces_ReturnsCorrectly(){
+        Player player = new Player(0,"Bob", PlayerColor.RED);
+
+        assertEquals("Bob", player.getName());
+    }
+
+    @Test
+    void getName_WithSpaces_ReturnsCorrectly(){
+        Player player = new Player(0,"Jane Doe", PlayerColor.RED);
+
+        assertEquals("Jane Doe", player.getName());
+    }
 }
