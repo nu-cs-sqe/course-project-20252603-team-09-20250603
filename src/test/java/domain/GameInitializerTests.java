b419b976cce3686f670f9152c30ceff253cb3772
crystalsychen <crystal.chen586@gmail.com>
2026-05-07 22:38:28 -0500

    Created tests for helper function validatePlayerCount()

---
+++ src/test/java/domain/GameInitializerTests.java
@@ -0,0 +1,36 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GameInitializerTests {
+    @Test
+    void twoPlayerCount_InvalidGame(){
+        GameInitializer initializer = new GameInitializer();
+
+        assertThrows(IllegalArgumentException.class, ()->initializer.validatePlayerCount(2));
+    }
+
+    @Test
+    void threePlayerCount_ValidGame(){
+        GameInitializer initializer = new GameInitializer();
+
+        assertDoesNotThrow(() -> initializer.validatePlayerCount(3));
+    }
+
+    @Test
+    void fourPlayerCount_ValidGame(){
+        GameInitializer initializer = new GameInitializer();
+
+        assertDoesNotThrow(() -> initializer.validatePlayerCount(4));
+    }
+
+    @Test
+    void fivePlayerCount_InvalidGame(){
+        GameInitializer initializer = new GameInitializer();
+
+        assertThrows(IllegalArgumentException.class, () -> initializer.validatePlayerCount(5));
+    }
+}
