8404ecfdbaabf8bff100066ca7bf53c9492f1ae1
crystalsychen <crystal.chen586@gmail.com>
2026-05-21 19:18:40 -0500

    created Game class with just constructor

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,4 +1,15 @@
 package domain;
 
+import java.util.List;
+
 public class Game {
+    private Board board;
+    private final List<Player> players;
+
+    public Game(Board board, List<Player> players){
+        this.board = board;
+        this.players = players;
+    }
+
+
 }
