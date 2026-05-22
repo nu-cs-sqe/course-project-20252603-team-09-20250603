256b80c88c39c78c58b762fd447f92f112baccaf
crystalsychen <crystal.chen586@gmail.com>
2026-05-21 19:19:13 -0500

    Initialized game in main only with board and player setup

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,4 +1,19 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        GameInitializer initializer = new GameInitializer();
+
+        List<String> playerNames = Arrays.asList(args);
+
+        Game game = initializer.setupGame(playerNames);
+
+        System.out.println("Game created successfully!");
+
+        //TODO: game.play()
+    }
 }
+
