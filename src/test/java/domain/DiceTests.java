cade8fdc46c6486abd06747b7fe770d87884bfdb
Aria Shi <ariashi15@gmail.com>
2026-05-23 16:36:00 -0500

    rollAndGetDieSum_Roll1And1_Return2 passes

--- src/test/java/domain/DiceTests.java
+++ src/test/java/domain/DiceTests.java
@@ -1,4 +1,29 @@
 package domain;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DiceTests {
+    @Test
+    public void rollAndGetDieSum_Roll1And1_Return2() {
+        Random mockRandom = EasyMock.createMock(Random.class);
+
+        EasyMock.expect(mockRandom.nextInt(6)).andReturn(0);
+        EasyMock.expect(mockRandom.nextInt(6)).andReturn(0);
+
+        EasyMock.replay(mockRandom);
+
+        Dice dice = new Dice(mockRandom);
+        dice.roll();
+
+        assertEquals(1, dice.getDie1());
+        assertEquals(1, dice.getDie2());
+        assertEquals(2, dice.getDieSum());
+
+        EasyMock.verify(mockRandom);
+    }
 }
