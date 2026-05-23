58f42f123414d105c5c87260286f63b29526ac7c
Aria Shi <ariashi15@gmail.com>
2026-05-23 16:39:50 -0500

    updated Game constructor

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -3,13 +3,15 @@ package domain;
 import java.util.List;
 
 public class Game {
-    private Board board;
+    private final Board board;
     private final List<Player> players;
+    private final Dice dice;
+    private final TurnManager turnManager;
 
-    public Game(Board board, List<Player> players){
+    public Game(Board board, List<Player> players, Dice dice, TurnManager turnManager){
         this.board = board;
         this.players = players;
+        this.dice = dice;
+        this.turnManager = turnManager;
     }
-
-
 }
