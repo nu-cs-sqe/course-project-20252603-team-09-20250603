ae1ef619df055d8f415e7034f669f467da2777da
crystalsychen <crystal.chen586@gmail.com>
2026-05-24 20:43:48 -0500

    Added placementValidator checking to ensure correct building and initalized start() method

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -7,13 +8,19 @@ public class Game {
     private final List<Player> players;
     private final Dice dice;
     private final TurnManager turnManager;
+    private final PlacementValidator placementValidator;
     // TODO: add devCardDeck to constructor
 
     public Game(Board board, List<Player> players, Dice dice, TurnManager turnManager) {
         this.board = board;
-        this.players = players;
+        this.players = new ArrayList<>(players);
         this.dice = dice;
         this.turnManager = turnManager;
+        this.placementValidator = new PlacementValidator(board);
+    }
+
+    public boolean start(){
+        return true;
     }
 
     public void build(Player currentPlayer, BuildType buildType, int locationId) {
@@ -31,6 +38,12 @@ public class Game {
             edge.buildRoad(currentPlayer);
         } else if (buildType == BuildType.SETTLEMENT){
             Node node = board.getNode(locationId);
+
+            try {
+                placementValidator.validateSettlementPlacement(node);
+            }catch (IllegalPlacementException exception) {
+                throw new IllegalStateException(exception.getMessage(), exception);
+            }
             node.buildSettlement(currentPlayer);
         } else if(buildType == BuildType.CITY){
             Node node = board.getNode(locationId);
