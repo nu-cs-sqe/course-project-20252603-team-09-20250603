22e09a92f2254c6fd20f53cdbfd3aed37cb0761c
Chloe Lu <chloelu2027@u.northwestern.edu>
2026-05-25 19:17:54 -0500

    test_TurnOfPurchase_ThrowsException() passes

--- src/main/java/domain/DevCard.java
+++ src/main/java/domain/DevCard.java
@@ -1,4 +1,23 @@
 package domain;
 
 public class DevCard {
+    private final DevCardType type;
+    private boolean isActive;
+
+    public DevCard(DevCardType type) {
+        this.type = type;
+        this.isActive = false;
+    }
+
+    public boolean getIsActive() {
+        return this.isActive;
+    }
+
+    public void doDevCardAction(Player player, Board board) {
+        if (!this.isActive) {
+            throw new IllegalActionException("Development cards cannot be played on the turn they were bought.");
+        }
+
+        this.isActive = false;
+    }
 }
--- src/test/java/domain/DevCardTests.java
+++ src/test/java/domain/DevCardTests.java
@@ -1,4 +1,26 @@
 package domain;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DevCardTests {
+
+    @Test // TC-DC-01
+    void test_TurnOfPurchase_ThrowsException() {
+        DevCard card = new DevCard(DevCardType.KNIGHT);
+
+        Player mockPlayer = EasyMock.createMock(Player.class);
+        Board mockBoard = EasyMock.createMock(Board.class);
+
+        assertFalse(card.getIsActive());
+
+        EasyMock.replay(mockPlayer, mockBoard);
+
+        assertThrows(IllegalActionException.class, () -> {
+            card.doDevCardAction(mockPlayer, mockBoard);
+        });
+
+        EasyMock.verify(mockPlayer, mockBoard);
+    }
 }
