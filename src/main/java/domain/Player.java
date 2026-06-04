a6b006a63a48461f6667e6c3065fffee829d1031
Chloe Lu <chloelu2027@u.northwestern.edu>
2026-06-04 18:45:34 -0500

    fix merge conflict

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -143,26 +143,6 @@ public class Player {
         return true;
     }
 
-
-
-    public void addVictoryPoints(int points) {
-        if (points < 0) {
-            throw new IllegalArgumentException("Points to add cannot be negative.");
-        }
-        this.victoryPoints += points;
-    }
-
-    public void removeVictoryPoints(int points) {
-        if (points < 0) {
-            throw new IllegalArgumentException("Points to remove cannot be negative.");
-        }
-        if (this.victoryPoints - points < 0) {
-            this.victoryPoints = 0;
-        } else {
-            this.victoryPoints -= points;
-        }
-    }
-
     public List<DevCard> getDevCardHand() { return this.devHand; }
 
     public void setDevCardHand(DevCard devCard) {
